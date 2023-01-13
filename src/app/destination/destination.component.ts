ede0adb70f8d3f1a105a5daf45460d3669e4fc1a
momchilmitev <momchilmitev87@gmail.com>
2023-01-13 12:30:55 +0100

    added some path aliases and added the logic for geting the destinations in the destination component

--- src/app/destination/destination.component.ts
+++ src/app/destination/destination.component.ts
@@ -1,14 +1,26 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { DestinationService } from'@services'
+import { Destination } from '@types'
 
 @Component({
   selector: 'app-destination',
   templateUrl: './destination.component.html',
   styleUrls: ['./destination.component.css']
 })
-export class DestinationComponent {
-  constructor (@Inject(DOCUMENT) private document: any) {
+export class DestinationComponent implements OnInit {
+  destinstions: Destination[] = [];
+
+  constructor (@Inject(DOCUMENT) private document: any, private destinationService: DestinationService) {
     this.document.body.classList = '';
     this.document.body.classList.add('destination')
+  }
+  ngOnInit (): void {
+    this.getDestinations();
+  }
+
+  getDestinations(): void {
+    this.destinstions = this.destinationService.getDestinations();
   }
 }
