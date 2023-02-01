3e1ba92fe076013075becbd9097f09e59927a9e4
momchilmitev <momchilmitev87@gmail.com>
2023-02-01 18:17:59 +0100

    added the logic for get local crew data and the logic for chnaging the current member

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -1,16 +1,38 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+
+import { CrewService } from "@services";
+import { Member } from "@types";
 
 @Component({
   selector: 'app-crew',
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
 })
-export class CrewComponent {
+export class CrewComponent implements OnInit {
+  crew: Member[] = [];
+  currentMember!: Member;
+  currentIndex: number = 0;
+
   constructor(
-    @Inject(DOCUMENT) private document: any
+    @Inject(DOCUMENT) private document: any,
+    private crewService: CrewService
   ){
     this.document.body.classList = '';
-    this.document.body.classList.add('crew')
+    this.document.body.classList.add('crew');
+  }
+
+  ngOnInit (): void {
+    this.getCrew();
+    this.currentMember = this.crew[this.currentIndex];
+  }
+
+  getCrew(): void {
+    this.crew = this.crewService.getCrew();
+  }
+
+  changeMember(index: number): void {
+    this.currentMember = this.crew[index];
+    this.currentIndex = index;
   }
 }
